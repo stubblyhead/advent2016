51490cd1f7c4274793aa2c01df06bd2837b59ced
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-16 11:43:32 -0800

    keypad class

---
+++ day2/keypad.rb
@@ -0,0 +1,89 @@
+class Keypad
+  attr_reader :button
+
+  def initialize
+    @button = 5
+  end
+
+  def next(directions)
+    directions.each_char do |i|
+      case @button
+      when 1
+        case i
+        when 'R'
+          @button = 2
+        when 'D'
+          @button = 4
+        end
+      when 2
+        case i
+        when 'R'
+          @button = 3
+        when 'L'
+          @button = 1
+        when 'D'
+          @button = 5
+        end
+      when 3
+        case i
+        when 'L'
+          @button = 2
+        when 'D'
+          @button = 6
+        end
+      when 4
+        case i
+        when 'R'
+          @button = 5
+        when 'U'
+          @button = 1
+        when 'D'
+          @button = 7
+        end
+      when 5
+        case i
+        when 'R'
+          @button = 6
+        when 'L'
+          @button = 4
+        when 'D'
+          @button = 8
+        when 'U'
+          @button = 2
+        end
+      when 6
+        case i
+        when 'U'
+          @button = 3
+        when 'L'
+          @button = 5
+        when 'D'
+          @button = 9
+        end
+      when 7
+        case i
+        when 'U'
+          @button = 4
+        when 'R'
+          @button = 8
+        end
+      when 8
+        case i
+        when 'R'
+          @button = 9
+        when 'L'
+          @button = 7
+        when 'U'
+          @button = 5
+        end
+      when 9
+        case i
+        when 'U'
+          @button = 6
+        when 'L'
+          @button = 8
+        end
+      end
+    end
+  end
+end
