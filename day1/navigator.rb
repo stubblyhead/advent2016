0268c9e5e81b2a0bb883a6d50004d9c9b4e3a774
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-15 19:53:18 -0800

    walker class

---
+++ day1/navigator.rb
@@ -0,0 +1,35 @@
+class Walker
+  attr_reader :x, :y, :directon
+
+  def initialize
+    @x = 0
+    @y = 0
+    @direction = :north
+  end
+
+  def walk(instr)
+    turn = instr[0]
+    distance = instr[1..-1].to_i
+    case @direction
+    when :north
+      turn == 'R' ? @direction = :east : @direction = :west
+    when :east
+      turn == 'R' ? @direction = :south : @direction = :north
+    when :south
+      turn == 'R' ? @direction = :west : @direction = :east
+    when :west
+      turn == 'R' ? @direction = :north : @direction = :south
+    end
+
+    case @direction
+    when :north
+      @y += distance
+    when :east
+      @x += distance
+    when :south
+      @y -= distance
+    when :west
+      @x -= distance
+    end
+  end
+end
