f552870372ae0b6eb65746cff72b55a044ede922
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-15 22:34:10 -0800

    recording each step

--- day1/navigator.rb
+++ day1/navigator.rb
@@ -1,10 +1,11 @@
 class Walker
-  attr_reader :x, :y, :directon
+  attr_reader :x, :y, :directon, :locations
 
   def initialize
     @x = 0
     @y = 0
     @direction = :north
+    @locations = [[0,0]]
   end
 
   def walk(instr)
@@ -23,23 +24,34 @@ class Walker
 
     case @direction
     when :north
+      (@y+1..@y+distance).each { |i| @locations.push([@x, i]) }
       @y += distance
     when :east
+      (@x+1..@x+distance).each { |i| @locations.push([i, @y]) }
       @x += distance
     when :south
+      templocations = []
+      (@y-distance..@y-1).each { |i| templocations.push([@x, i]) }
+      @locations += templocations.reverse
       @y -= distance
     when :west
+      templocations = []
+      (@x-distance..@x-1).each { |i| templocations.push([i, @y]) }
+      @locations += templocations.reverse
       @x -= distance
     end
   end
 end
 
 dirs = []
+locations = []
 
 File.open('./input') { |file| dirs = file.readline.chomp.split(', ') }
 
+dirs = %w[R8 R4 R4 R8]
+
 hq = Walker.new()
 
-dirs.each { |i| hq.walk(i) }
-
-puts hq.x.abs + hq.y.abs
+dirs.each do |i|
+  hq.walk(i)
+end
