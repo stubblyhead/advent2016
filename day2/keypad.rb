1268a249b800822d9be9ffc296693a797c389d4d
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-16 15:21:11 -0800

    making sequence a string instead of array

--- day2/keypad.rb
+++ day2/keypad.rb
@@ -2,91 +2,91 @@ class Keypad
   attr_reader :button, :sequence
 
   def initialize
-    @button = 5
-    @sequence = []
+    @button = '5'
+    @sequence = ''
   end
 
   def next(directions)
     directions.each_char do |i|
       case @button
-      when 1
+      when '1'
         case i
         when 'R'
-          @button = 2
+          @button = '2'
         when 'D'
-          @button = 4
+          @button = '4'
         end
-      when 2
+      when '2'
         case i
         when 'R'
-          @button = 3
+          @button = '3'
         when 'L'
-          @button = 1
+          @button = '1'
         when 'D'
-          @button = 5
+          @button = '5'
         end
-      when 3
+      when '3'
         case i
         when 'L'
-          @button = 2
+          @button = '2'
         when 'D'
-          @button = 6
+          @button = '6'
         end
-      when 4
+      when '4'
         case i
         when 'R'
-          @button = 5
+          @button = '5'
         when 'U'
-          @button = 1
+          @button = '1'
         when 'D'
-          @button = 7
+          @button = '7'
         end
-      when 5
+      when '5'
         case i
         when 'R'
-          @button = 6
+          @button = '6'
         when 'L'
-          @button = 4
+          @button = '4'
         when 'D'
-          @button = 8
+          @button = '8'
         when 'U'
-          @button = 2
+          @button = '2'
         end
-      when 6
+      when '6'
         case i
         when 'U'
-          @button = 3
+          @button = '3'
         when 'L'
-          @button = 5
+          @button = '5'
         when 'D'
-          @button = 9
+          @button = '9'
         end
-      when 7
+      when '7'
         case i
         when 'U'
-          @button = 4
+          @button = '4'
         when 'R'
-          @button = 8
+          @button = '8'
         end
-      when 8
+      when '8'
         case i
         when 'R'
-          @button = 9
+          @button = '9'
         when 'L'
-          @button = 7
+          @button = '7'
         when 'U'
-          @button = 5
+          @button = '5'
         end
-      when 9
+      when '9'
         case i
         when 'U'
-          @button = 6
+          @button = '6'
         when 'L'
-          @button = 8
+          @button = '8'
         end
       end
     end
-    @sequence.push(@button)
+    @sequence += @button
   end
 end
 
