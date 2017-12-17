56f105a30c78ab2a6c2a91080ba5c4be50126455
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-17 05:55:07 -0800

    finished day 3

--- day3/triangles.rb
+++ day3/triangles.rb
@@ -1,4 +1,18 @@
 def is_triangle?(lengths)
-  lengths.sort!
-  return lengths[2] < (lengths[0] + lengths[1])
+  return (lengths[2].to_i < (lengths[0].to_i + lengths[1].to_i) && lengths[1].to_i < (lengths[0].to_i + lengths[2].to_i) && lengths[0].to_i < (lengths[2].to_i + lengths[1].to_i) )
 end
+
+triangles = []
+File.open './input' do |file|
+  file.each_line { |line| triangles.push(line.chomp.split) }
+end
+
+triangles = triangles.transpose
+
+valid = 0
+triangles.each do |i|
+  while i != []
+    valid += 1 if is_triangle?(i.pop(3))
+  end 
+end
+puts "#{valid} potential triangles"
