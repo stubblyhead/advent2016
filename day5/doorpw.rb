e644704f12d3c353b364f5e69987c2515140937a
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-25 12:12:07 -0800

    part 2 test passed

--- day5/doorpw.rb
+++ day5/doorpw.rb
@@ -2,13 +2,15 @@ require 'digest'
 
 door_id = 'abc'
 index = 0
-count = 0
+password = '        '
 
-until count == 8
+while password.index(' ')
   md5sum = Digest::MD5.hexdigest(door_id + index.to_s)
-  if md5sum[0..4] == '00000'
-    print md5sum[5]
-    count += 1
+  if md5sum[0..4] == '00000' and md5sum[5].match(/[0-7]/) and password[md5sum[5].to_i] == ' '
+    password[md5sum[5].to_i] = md5sum[6]
+    puts "putting #{md5sum[6]} in slot #{md5sum[5]}"
   end
   index += 1
 end
+
+puts password
