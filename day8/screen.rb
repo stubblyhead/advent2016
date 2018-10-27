9ceb1dd3928dd24dd453de885574ebcc4d97d3c5
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-10-27 11:03:42 -0700

    test case working

--- day8/screen.rb
+++ day8/screen.rb
@@ -25,4 +25,27 @@ class Screen
     temp_col.rotate!(-shift)
     @pixels.each_index { |i| @pixels[i][col] = temp_col[i] }
   end
+
+  def count_lights
+    lights = 0
+    @pixels.each { |i| lights += i.count('#') }
+    lights
+  end
 end
+
+instructions = File.readlines('./testcase', chomp: true)
+lcd = Screen.new(3,7)
+instructions.each do |line|
+  parts = line.split
+  case parts[0]
+  when 'rect'
+    args = parts[1].split('x')
+    lcd.rect(args[0].to_i, args[1].to_i)
+  when 'rotate'
+    args = parts[1,4]
+    lcd.send("rotate_#{args[0]}", args[1].match(/\d+/)[0].to_i, args[3].to_i)
+  end
+end
+
+lcd.print
+puts "#{lcd.count_lights} pixels lighted"
