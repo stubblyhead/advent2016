976b7d27a5d75426f10fcff5be68916109de07e7
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-10-26 20:00:16 -0700

    was using wrong array when removing false matches

--- day7/ipv7.rb
+++ day7/ipv7.rb
@@ -5,12 +5,12 @@ addresses.each do |line|
   matches.push(line) if line.match(/(\w)(\w)\2\1/)
 end
 
-addresses.each do |line|
-  addresses.delete(line) if line.match(/(\w)\1\1\1/)
+matches.each do |line|
+  matches.delete(line) if line.match(/(\w)\1\1\1/)
 end
 
-addresses.each do |line|
-  addresses.delete(line) if line.match(/\[.*(\w)(\w)\2\1.*\]/)
+matches.each do |line|
+  matches.delete(line) if line.match(/\[.*(\w)(\w)\2\1.*\]/)
 end
 
-puts addresses.length
+puts "#{matches.length} TLS addresses"
