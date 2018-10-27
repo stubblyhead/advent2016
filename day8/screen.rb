f9c31648536982f445e19e4902f68a9de658e25f
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-10-27 10:31:16 -0700

    basic screen class

---
+++ day8/screen.rb
@@ -0,0 +1,28 @@
+class Screen
+  def initialize(rows, cols)
+    @pixels = Array.new(rows) { Array.new(cols) { '.' } }
+  end
+
+  def print
+    @pixels.each { |i| puts i.join }
+  end
+
+  def rect(cols, rows)
+    (0..rows-1).each do |i|
+      (0..cols-1).each do |j|
+        @pixels[i][j] = '#'
+      end
+    end
+  end
+
+  def rotate_row(row, shift)
+    @pixels[row].shift!(shift - 1)
+  end
+
+  def rotate_column(col, shift)
+    temp_col = []
+    @pixels.each { |i| temp_col.push(i[col]) }
+    temp_col.shift!(shift-1)
+    @pixels.each_index { |i| @pixels[i][col] = temp_col[i] }
+  end
+end
