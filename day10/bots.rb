0a7087bbbba6e1f2e0b1183fdd6edd689aeecdce
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-10-31 14:17:12 -0700

    rejiggered the bot class a little and changing how I go through the instructions

--- day10/bots.rb
+++ day10/bots.rb
@@ -1,14 +1,14 @@
 class Bot
-  attr_reader :chips, :instructions, :id
+  attr_reader :chips, :id
 
-  def initialize(id, instructions)
-    @instructions = instructions
+  def initialize(id)
     @chips = []
     @id = id
   end
 
   def take_value(value)
     @chips.push(value)
+    watch_value
   end
 
   def give_value(value, dest)
@@ -17,7 +17,7 @@ class Bot
   end
 
   def watch_value
-    if @chips.index(61) and @chips.index(17)
+    if @chips.index(2) and @chips.index(5)
       puts "bot #{@id} reporting for duty!"
     end
   end
@@ -33,12 +33,57 @@ class Output
   def initialize
     @chips = []
   end
+
+  def take_value(value)
+    @chips.push(value)
+  end
 end
 
 
-all_instructions = File.readlines('./testcase', chomp: true)
-instructions_by_bot = {}
-all_instructions.each do |i|
-  if matched = i.match(/value (\d+) goes to bot (\d+)/)
-    instructions_by_bot[matched[1]].push(['grab_value', foo[2]])
-  elsif matched = i.match(/bot (\d+) gives low to (\w+) (\d+) and high to (\w+) (\d+)/)
+all_instructions = File.readlines('./testcase', chomp: true).sort.reverse
+last_grab = all_instructions.index { |i| i.match(/^bot.*/) }
+bots = {}
+outputs = {}
+all_instructions[0,last_grab].each do |i|
+  parts = i.split
+  value, bot = parts[1], parts[5]
+  if bots[bot] == nil
+    bots[bot] = Bot.new(bot)
+  end
+  bots[bot].grab_value(value)
+end
+
+while all_instructions.length > 0
+  all_instructions[last_grab..-1].each do |i|
+    parts = i.match(/bot (\d+) gives low to (\w+) (\d+) and high to (\w+) (\d+)/)
+    bot = parts[1]
+    low_dest_type = parts[2]
+    low_dest = parts[3]
+    high_dest_type = parts[4]
+    high_dest = parts[5]
+    next if bots[bot].chips.length < 2
+    if low_dest_type == 'output'
+      if outputs[low_dest] == nil
+        outputs[low_dest] = Output.new
+      end
+      bots[bot].give_value(bots[bot].min,outputs[low_dest])
+    else
+      if bots[low_dest] == nil
+        bots[low_dest] = Bot.new(low_dest)
+      end
+      bots[bot].give_value(bots[bot].min,bots[low_dest])
+    end
+    if high_dest_type 'output'
+      if outputs[high_dest] == nil
+        outputs[high_dest] = Output.new
+      end
+      bots[bot].give_value(bots[bot].max,outputs[low_dest])
+    else
+      if bots[bot] == nil
+        bots[bot] = Bot.new(bot)
+      end
+      bots[bot].give_value(bots[bot].max,bots[low_dest])
+    end
+    all_instructions.delete(parts[0])
+  end
+end
