4a78841b47204bf4d5072448db61b30899a35d1e
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-01 16:09:44 -0700

    buildling out maze correctly

--- day13/zork.rb
+++ day13/zork.rb
@@ -1,18 +1,18 @@
 require 'matrix'
 require 'pry'
-
+binding.pry
 class Maze
   attr_reader :layout, :fav_num
 
   def initialize(fav_num)
     @fav_num = fav_num
-    @layout = Matrix[Array.new(10) { Array.new(10) } ]
-    @layout.each do |y|
-      y.each do |x|
+    @layout = Array.new(10) { Array.new(10) }
+    @layout.each_index do |y|
+      @layout[y].each_index do |x|
         value = x**2 + 3*x + 2*x*y + y + y**2 + @fav_num
         ones = 0
         Math.log(value, 2).ceil.downto(0) { |i| ones += value[i] }
-        ones.even? ? @layout[y,x] = '.' : @layout[y,x] = '#'
+        ones.even? ? @layout[y][x] = '.' : @layout[y][x] = '#'
       end
     end
   end
@@ -21,3 +21,12 @@ class Maze
     @layout.each { |i| puts i.join }
   end
 end
+
+unless ARGV[0]
+  puts "please pass the favorite number as the first argument"
+  exit
+end
+
+cubicles = Maze.new(ARGV[0].to_i)
+
+cubicles.print_layout
