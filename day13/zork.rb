9c2937925d8ac283f401110b65febc1f63cf9bad
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-02 12:36:43 -0700

    function to determine next direction and the move in that direction

--- day13/zork.rb
+++ day13/zork.rb
@@ -1,12 +1,14 @@
 require 'matrix'
 require 'pry'
+
 binding.pry
+
 class Maze
-  attr_reader :layout, :fav_num
+  attr_reader :layout, :fav_num, :pos_x, :pos_y, :direction
 
   def initialize(fav_num)
     @fav_num = fav_num
-    @layout = Array.new(10) { Array.new(10) }
+    @layout = Array.new(50) { Array.new(50) }
     @layout.each_index do |y|
       @layout[y].each_index do |x|
         value = x**2 + 3*x + 2*x*y + y + y**2 + @fav_num
@@ -15,10 +17,51 @@ class Maze
         ones.even? ? @layout[y][x] = '.' : @layout[y][x] = '#'
       end
     end
+    @pos_x, @pos_y = [1,1]
+    @layout[1][1] = 'X'
+    @direction = :south
   end
 
   def print_layout
     @layout.each { |i| puts i.join }
+  end
+
+  def next_direction
+    local = [@layout[@pos_y-1][@pos_x-1,3],
+             @layout[@pos_y][@pos_x-1,3],
+             @layout[@pos_y+1][@pos_x-1,3]]
+    case @direction
+    when :north
+      @direction = :east; break if local[2][2] == '#' and local[1][2] == '.'
+      @direction = :west; break if local[0][1] == '#' and local[1][0] == '.'
+      @direction = :south if local[0][1] == '#'
+    when :east
+      @direction = :south; break if local[2][0] == '#' and local[2][1] == '.'
+      @direction = :north; break if local[2][1] == '#' and local[0][1] == '.'
+      @direction = :west if local[2][1] == '#'
+    when :south
+      @direction = :west; break if local[0][0] == '#' and local[1][0] == '.'
+      @direction = :east; break if local[2][1] == '#' and local[1][2] == '.'
+      @direction = :north if local[2][1] == '#'
+    when :west
+      @direction = :north; break if local[0][2] == '#' and local[0][1] == '.'
+      @direction = :south; break if local[1][0] == '#' and local[2][1] == '.'
+      @direction = :east if local[1][0] == '#'
+    end
+  end
+
+  def move
+    next_diretion
+    case @direction
+    when :north
+      @pos_y -= 1
+    when :east
+      @pos_x += 1
+    when :south
+      @pos_y += 1
+    when :west
+      @pos_x -= 1
+    end
   end
 end
 
@@ -29,4 +72,5 @@ end
 
 cubicles = Maze.new(ARGV[0].to_i)
 
+10.times { cubicles.move }
 cubicles.print_layout
