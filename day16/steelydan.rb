c5e5a99c79c9ae4193f9824092dfe6f47f7be1c0
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-05 12:34:44 -0800

    part 1 complete

--- day16/steelydan.rb
+++ day16/steelydan.rb
@@ -1,3 +1,6 @@
+require 'pry'
+binding.pry
+
 def expand(a)
   b = a.reverse
   b.chars.each_index do |i|
@@ -5,3 +8,32 @@ def expand(a)
   end
   return a + '0' + b
 end
+
+def checksum(a)
+  count = 0
+  pairs = []
+  while count < a.length
+    pairs.push(a[count] + a[count + 1])
+    count += 2
+  end
+  value = ''
+  pairs.each do |i|
+    i[0] == i[1] ? value += '1' : value += '0'
+  end
+
+  value.length.even? ? checksum(value) : value
+end
+
+
+
+length = 272
+
+input = '00111101111101000'
+dummy = input
+until dummy.length >= length
+  dummy = expand(dummy)
+  puts dummy.length
+end
+dummy = dummy[0,length]
+
+puts checksum(dummy)
