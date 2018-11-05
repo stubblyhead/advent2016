405d93959fd45677ff61bf2ce553a5565d1fa6de
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-05 10:36:16 -0800

    put a one-position spinner at the top for better visualization, some code for finding patterns

--- day15/spinners.rb
+++ day15/spinners.rb
@@ -11,3 +11,24 @@ class Spinner
   end
 
 end
+
+
+layers = []
+layers[0] = Spinner.new(1,0)
+layers[1] = Spinner.new(5,4)
+layers[2] = Spinner.new(2,1)
+time = 0
+print "time 0 "
+layers.each { |i| print "#{i.current} " }
+print "\n"
+
+while true
+  time += 1
+  print "time #{time} "
+  layers.each do |i|
+    i.tick
+    print "#{i.current} "
+  end
+  print "\n"
+  sleep 1
+end
