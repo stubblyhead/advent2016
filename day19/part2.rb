625b3c012e524c7af47d20fbccdc8ebd615423bb
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-07 11:14:04 -0800

    need to do more modulo arithmetic and adjust current index down if removing something backwards in the array

--- day19/part2.rb
+++ day19/part2.rb
@@ -1,14 +1,20 @@
+require 'pry'
+
+binding.pry
+
 def part2(n, myturn)
   if n.length == 1
     n[0]
   else
-    n.delete_at(myturn + (n.length/2).floor )
+    myturn_value = n[myturn]
+    n.delete_at((myturn + n.length / 2) % n.length)
+    myturn = n.index(myturn_value)
     part2(n, (myturn + 1) % n.length)
   end
 end
 
-(1..10).each do |i|
+(1..100).each do |i|
   circle = Array.new(i)
   circle.each_index { |i| circle[i] = i+1 }
   puts "#{i}\t#{part2(circle,0)}"
-end    
+end
