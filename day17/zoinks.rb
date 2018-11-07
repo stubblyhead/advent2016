54eed5c0b11aad6db23a6e04802f8de067c2a828
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-06 16:37:32 -0800

    whoops should have committed somethign before now.  built out some search, i think it's stuck in an infinite loop though

---
+++ day17/zoinks.rb
@@ -0,0 +1,106 @@
+require 'digest'
+require 'pry'
+
+binding.pry
+
+class Room
+  attr_reader :doors
+
+  def initialize(up, down, left, right)
+    @doors = {}
+    @doors[:up] = up
+    @doors[:down] = down
+    @doors[:left] = left
+    @doors[:right] = right
+  end
+
+  def lock(door)
+    @doors[door] = :locked unless @doors[door] == :wall
+  end
+
+  def unlock(door)
+    @doors[door] = :unlocked unless @doors[door] == :wall
+  end
+
+  def set_doors(code)
+    open_char = /[b-f]/
+    @doors.keys.each { |i| lock(@doors[i]) }
+    unlock(@doors[:up]) if code[0].match(open_char)
+    unlock(@doors[:down]) if code[1].match(open_char)
+    unlock(@doors[:left]) if code[2].match(open_char)
+    unlock(@doors[:right]) if code[3].match(open_char)
+  end
+
+  def find_moves
+    @doors.keys.map { |i| i if @doors[i] == :unlocked }.compact
+  end
+
+
+
+end
+
+class Maze
+  attr_reader :rooms, :passcode
+
+  def initialize(passcode)
+    @passcode = passcode
+    @rooms = Array.new(4) { Array.new(4) }
+    @rooms[0][0] = Room.new(:wall, :unlocked, :wall, :unlocked)
+    @rooms[0][1] = Room.new(:wall, :unlocked, :unlocked, :unlocked)
+    @rooms[0][2] = Room.new(:wall, :unlocked, :unlocked, :unlocked)
+    @rooms[0][3] = Room.new(:wall, :unlocked, :unlocked, :wall)
+    @rooms[1][0] = Room.new(:unlocked, :unlocked, :wall, :unlocked)
+    @rooms[1][1] = Room.new(:unlocked, :unlocked, :unlocked, :unlocked)
+    @rooms[1][2] = Room.new(:unlocked, :unlocked, :unlocked, :unlocked)
+    @rooms[1][3] = Room.new(:unlocked, :unlocked, :unlocked, :wall)
+    @rooms[2][0] = Room.new(:unlocked, :unlocked, :wall, :unlocked)
+    @rooms[2][1] = Room.new(:unlocked, :unlocked, :unlocked, :unlocked)
+    @rooms[2][2] = Room.new(:unlocked, :unlocked, :unlocked, :unlocked)
+    @rooms[2][3] = Room.new(:unlocked, :unlocked, :unlocked, :wall)
+    @rooms[3][0] = Room.new(:unlocked, :wall, :wall, :unlocked)
+    @rooms[3][1] = Room.new(:unlocked, :wall, :unlocked, :unlocked)
+    @rooms[3][2] = Room.new(:unlocked, :wall, :unlocked, :unlocked)
+    @rooms[3][3] = Room.new(:unlocked, :wall, :unlocked, :wall)
+  end
+
+end
+
+passcode = 'ihgpwlah'
+mansion = Maze.new(passcode)
+#roomhash = Digest::MD5.hexdigest(mansion.passcode)[0,4]
+#Maze.rooms[0][0].set_doors(roomhash)
+
+def bfs(mansion)
+  open_set = []
+  root = [0,0,mansion.passcode]
+  open_set.push(root)
+
+  until open_set.empty?
+    subtree_root = open_set.shift
+    room = subtree_root[0,2]
+    code = subtree_root[2]
+    hash = Digest::MD5.hexdigest(code)[0,4]
+    mansion.rooms[room[0]][room[1]].set_doors(hash)
+    if room == [3,3]
+      return hash[mansion.passcode.length..-1]
+    end
+
+    directions = mansion.rooms[room[0]][room[1]].find_moves
+    directions.each do |i|
+      nexthash = hash + i.to_s.upcase[0]
+      nextroom = room
+      if i == :up
+        nextroom[0] -= 1
+      elsif i == :down
+        nextroom[0] += 1
+      elsif i == :left
+        nextroom[1] -= 1
+      elsif i == :right
+        nextroom[1] += 1
+      end
+      open_set.push(nextroom + [nexthash])
+    end
+  end
+end
+
+puts bfs(mansion)
