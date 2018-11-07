9c37645124d7514d24be4113e5425dc088240de3
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-06 22:57:11 -0800

    this is the josephus problem with some added distractions

--- day19/loblolly.rb
+++ day19/loblolly.rb
@@ -1,12 +1,7 @@
-elves = Array.new(3018458)
+elves = (ARGV[0].to_i or 5)
 
-elves.each_index { |i| elves[i] = [i+1, 1] }
-
-until elves.length == 1
-  elves.each_index do |i|
-    elves[i][1] += elves[(i + 1) % elves.length][1]
-    elves.delete_at((i + 1) % elves.length)
-  end
+def josephus(elves)
+  return 2 * (elves - 2**(Math::log2(elves)).floor) + 1
 end
 
-puts elves[0][0]
+puts josephus(elves)
