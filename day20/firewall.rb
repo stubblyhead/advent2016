efdec0796bc31ab7a1dea014dcc79b305d140779
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-08 07:07:49 -0800

    keep adding the gap between the highest seen address and the lowest address of the next range

--- day20/firewall.rb
+++ day20/firewall.rb
@@ -10,12 +10,23 @@ ranges.each do |i|
   ruleshash[parts[0].to_i] = parts[1].to_i
 end
 
-highest_seen = 0
+allowed_addrs = 0
+highest_seen  = 0
+first_gap = 0
+
 ruleshash.keys.sort.each do |i|
   if i > highest_seen + 1
-    puts highest_seen + 1
-    break
-  else
-    highest_seen = ruleshash[i]
+    if first_gap == 0
+      first_gap = highest_seen + 1
+    end
+    allowed_addrs += i - highest_seen - 1
   end
+  highest_seen = ruleshash[i] unless highest_seen > ruleshash[i]
 end
+
+unless ruleshash.values.max == 4294967295
+  allowed_addrs += 4294967295 - ruleshash.values.max
+end
+
+puts "lowest allowed address is #{first_gap}"
+puts "#{allowed_addrs} total allowed addresses"
