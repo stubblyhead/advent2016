9886bec641d88709f2628da8a0a44abf226eff78
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-08 09:32:38 -0800

    new function to output password, logic to parse input and execute commands therein

--- day21/scrambler.rb
+++ day21/scrambler.rb
@@ -35,4 +35,39 @@ class Scrambler
     @password.remove_at(x)
     @password.insert(y, char)
   end
+
+  def get_password
+    @password.join
+  end
 end
+
+input = File.readlines('./testcase', :chomp => true)
+instructions = []
+input.each do |i|
+  parts = i.split
+  if parts[0] == 'swap'
+    if parts[1] == 'position'
+      instructions.push(['swap_position', parts[2].to_i, parts[-1].to_i])
+    else
+      instructions.push(['swap_letter', parts[2], parts[-1]])
+    end
+  elsif parts[0] == 'reverse'
+    instructions.push(['reverse', parts[2].to_i, parts[-1].to_i])
+  elsif parts[0] == 'rotate'
+    if parts[1] == 'based'
+      instructinos.push(['rotate_x', parts[-1]])
+    else
+      instructions.push(["rotate_#{parts[1]}", parts[2].to_i])
+    end
+  elsif parts[0] == 'move'
+    instructions.push(['move', parts[2].to_i, parts[-1].to_i])
+  end
+end
+
+password = Scrambler.new('abcde')
+
+instructions.each do |i|
+  password.send(*i)
+end
+
+puts password.get_password
