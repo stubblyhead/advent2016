61f1f58928f4bfeaeecf9080437a1a069390dde0
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-08 11:06:46 -0800

    made some undoing functions, something's still off

--- day21/scrambler.rb
+++ day21/scrambler.rb
@@ -36,6 +36,27 @@ class Scrambler
     @password.insert(y, char)
   end
 
+  def unrotate_x(x)
+    position = @password.index(x)
+    if position.odd?
+      rotate_left((position/2.0).ceil)
+    else
+      rotate_left(position + 2)
+    end
+  end
+
+  def unrotate_left(x)
+    rotate_right(x)
+  end
+
+  def unrotate_right(x)
+    rotate_left(x)
+  end
+
+  def unmove(x,y)
+    move(y,x)
+  end
+
   def get_password
     @password.join
   end
@@ -65,6 +86,34 @@ input.each do |i|
 end
 
 password = Scrambler.new('abcdefgh')
+
+instructions.each do |i|
+  password.send(*i)
+end
+
+scrambled_password = password.get_password
+
+instructions = []
+input.reverse.each do |i|
+  parts = i.split
+  if parts[0] == 'swap'
+    if parts[1] == 'position'
+      instructions.push(['swap_position', parts[2].to_i, parts[-1].to_i])
+    else
+      instructions.push(['swap_letter', parts[2], parts[-1]])
+    end
+  elsif parts[0] == 'reverse'
+    instructions.push(['reverse', parts[2].to_i, parts[-1].to_i])
+  elsif parts[0] == 'rotate'
+    if parts[1] == 'based'
+      instructions.push(['unrotate_x', parts[-1]])
+    else
+      instructions.push(["unrotate_#{parts[1]}", parts[2].to_i])
+    end
+  elsif parts[0] == 'move'
+    instructions.push(['unmove', parts[2].to_i, parts[-1].to_i])
+  end
+end
 
 instructions.each do |i|
   password.send(*i)
