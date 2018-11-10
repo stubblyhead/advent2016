81fb4e6327599c2ddbb3b7d34557efa24b1db510
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-11-10 14:30:13 -0800

    added new functions to moev goal and empty nodes at will and test equality of different layouts, and a what-if function for moves

--- day22/slide.rb
+++ day22/slide.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'deep_clone'
 binding.pry
 
 class Puzzle
@@ -24,7 +25,33 @@ class Puzzle
       end
     end
     @layout[0][-1] = 'G'
-    @goal = [-1,0]
+    @goal = [0,@layout[0].length-1]
+  end
+
+  # def initialize(layout, empty, goal)
+  #   @layout, @empty, @goal = layout, empty, goal
+  # end
+
+  def set_goal(x)
+    @layout[@goal[0]][@goal[1]] = '•'
+    @layout[x[0]][x[1]] = 'G'
+    @goal = x
+  end
+
+  def set_empty(x)
+    @layout[@empty[0]][@empty[1]] = '•'
+    @layout[x[0]][x[1]] = '_'
+    @empty = x
+  end
+
+  def ==(other)
+    if other.class == Puzzle and @layout.length == other.layout.length \
+       and @layout[0].length == other.layout[0].length and @empty == other.empty \
+       and @goal == other.goal
+      return true
+    else
+      return false
+    end
   end
 
   def print_layout
@@ -33,15 +60,38 @@ class Puzzle
 
   def find_moves
     moves = [:up,:down,:left,:right]
-    moves.delete(:up) if @empty[0] == 0 or @layout[@empty[0] -1 ][@empty[1]] == '#'
+    moves.delete(:up) if @empty[0] == 0 or @layout[@empty[0] - 1 ][@empty[1]] == '#'
     moves.delete(:down) if @empty[0] == @layout.length - 1 or @layout[@empty[0] + 1][@empty[1]] == '#'
     moves.delete(:left) if @empty[1] == 0
     moves.delete(:right) if @empty[1] == @layout[@empty[0]].length - 1 or @layout[@empty[0]][@empty[1] + 1] == '#'
     moves
   end
 
+  def get_next(source)
+    next_empty = @empty.clone
+    current_empty = @empty.clone
+    current_goal = @goal.clone
+    case source
+    when :up
+      next_empty[0] -= 1
+    when :down
+      next_empty[0] += 1
+    when :left
+      next_empty[1] -= 1
+    when :right
+      next_empty[1] += 1
+    end
+    if next_empty == current_goal
+      [next_empty, current_empty]
+    else
+      [next_empty, current_goal]
+    end
+  end
+
+
+
   def move(source)
-    @layout[@empty[0]][@empty[1]] = '•'
+    current = @empty
     case source
     when :up
       @empty[0] -= 1
@@ -52,6 +102,7 @@ class Puzzle
     when :right
       @empty[1] += 1
     end
+    @layout[current[0]][current[1]] = @layout[empty[0]][empty[1]]
     @layout[empty[0]][empty[1]] = '_'
   end
 
@@ -60,3 +111,22 @@ end
 
 lines = File.readlines('./input', :chomp=>true)
 mypuzzle = Puzzle.new(lines)
+true
+
+# def bfs(grid)
+#   open_set = []
+#   closed_set = []
+#   meta = {}
+#   root = [grid.empty,grid.goal]
+#   meta[root] = [nil,nil]
+#   open_set.push(root)
+#
+#   until open_set.empty?
+#     subtree_root = open_set.shift
+#     if subtree_root.goal = [0,0]
+#       return construct_path(subtree_root, meta)
+#     else
+#       temp = DeepClone.clone(grid)
+#       temp.set_empty(subtree_root[0])
+#       temp.set_goal(subtree_root[1])
+#       moves.each do |i|
